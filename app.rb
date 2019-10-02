bd03b3ec74eddf283bae2a60f13542b0dbaa2c20
Matt Stockman <mjjstockman@gmail.com>
2019-10-02 19:48:52 +0100

    refractor form to use post/redirect/get pattern

--- app.rb
+++ app.rb
@@ -2,15 +2,21 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   set :session_secret, 'super secret'
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    p params
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
